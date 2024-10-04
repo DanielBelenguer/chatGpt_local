7f1fc35f69cf55e7ce00f43c43dacf94167f51be
Daniel Belenguer <danielbelenguerfernandez@gmail.com>
2024-10-04 16:01:54 +0200

    arreglado el problema

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
-const engine = new MLCEngine()
-const handler = new MLCEngineWorkerHandler(engine)
 
-onmessage = msg => {
-    handler.onmessage(msg)
+const handler = new WebWorkerMLCEngineHandler()
+
+self.onmessage = (msg) => {
+  handler.onmessage(msg)
 }
